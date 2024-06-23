9b75fa4e43f501e585e46a04c45f2753ea040002
TheMaxium69 <mxm.tournier@gmail.com>
2024-06-23 17:45:37 +0200

    final

--- main.js
+++ main.js
@@ -11,8 +11,11 @@ function getSearchParams() {
     for (let param of searchParams) {
         params[param[0]] = param[1];
     }
-    searchBar.value = params['r']
-    sendSearchRequest(params['r']);
+    if (params['r'] !== undefined){
+
+        searchBar.value = params['r']
+        sendSearchRequest(params['r']);
+    }
 
 }
 
@@ -37,16 +40,51 @@ function addNewMap(data) {
     var mapFusion = document.getElementById('fusion');
     mapFusion.innerHTML = null;
 
-    data.forEach(fusion => {
-        var mapFusion = document.getElementById('fusion');
-        mapFusion.innerHTML += `
-        <div class="map">
-            <h4 class="tierName">Tier ${fusion.tier}</h4>
-            <img class="item item1" src="textures/items/${fusion.item1}" title="${fusion.item1}" href="">
-            <img class="item item2" src="textures/items/${fusion.item2}" title="${fusion.item2}">
-            <img class="item itemResult" src="textures/items/${fusion.itemResult}" title="${fusion.itemResult}">
-            <img class="item itemFuel" src="textures/items/${fusion.itemFuel}" title="${fusion.itemFuel}">
-        </div>
-        `;
+    var infoItem = document.getElementById('infoItem');
+    infoItem.innerHTML = null;
+
+
+
+    var i = 1;
+
+    data.forEach(reponse => {
+        if (i == 1){
+
+            infoItem.innerHTML += `
+            <div id="firstInfoItem">
+                <img class="itemInfo" src="textures/items/${reponse['name']}.png">
+            </div>
+            <div id="secondInfoItem">
+                <h3>${reponse['name']} <span>#${reponse['tyroid']}</span></h3>
+                <p>Tier : ${reponse['tier']}</p>
+                <p>Version : ${reponse['version']}</p>
+                <p>Nombre de Fusion : ${reponse['fusion_count']}</p>
+                <p>Combustible : ${reponse['isFuel']}</p>
+            </div>
+            `;
+
+
+
+        } else {
+
+            reponse.forEach(fusion => {
+
+                    mapFusion.innerHTML += `
+            <div class="map">
+                <h4 class="tierName">Tier ${fusion.tier}</h4>
+                <img class="item item1" src="textures/items/${fusion.item1}" title="${fusion.item1}" onclick="window.location.href = '?r=${fusion.item1}';">
+                <img class="item item2" src="textures/items/${fusion.item2}" title="${fusion.item2}" onclick="window.location.href = '?r=${fusion.item2}';">
+                <img class="item itemResult" src="textures/items/${fusion.itemResult}" title="${fusion.itemResult}" onclick="window.location.href = '?r=${fusion.itemResult}';">
+                <img class="item itemFuel" src="textures/items/${fusion.itemFuel}" title="${fusion.itemFuel}" onclick="window.location.href = '?r=${fusion.itemFuel}';">
+            </div>
+            `;
+
+            });
+
+        }
+
+
+
+        i++;
     });
 }
